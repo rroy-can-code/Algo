384b052c42debc67cee43ab4beb46d1e736588c8
Rahul17593 <rahulroy17593@gmail.com>
2019-12-08 14:48:15 +0530

    "Bubble Sort algorithm"

---
+++ src/main/java/com/practice/algorithm/sorting/Main.java
@@ -0,0 +1,15 @@
+package com.practice.algorithm.sorting;
+
+public class Main {
+	public static void main(String[] args) {
+
+		int arr[] = new int[] { 6, 1, 8, 7, 3, 13, 9 };
+		BubbleSort.bubbleSort(arr);
+		System.out.print("After Sorting : [");
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + " ");
+		}
+		System.out.print("]");
+	}
+
+}
