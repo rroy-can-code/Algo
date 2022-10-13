93a4b7b68b8a9d8b23323d66e576a61d7a83c2a8
Rahul Roy <rahulroy17593@gmail.com>
2022-10-14 00:19:56 +0530

    Max length substring

---
+++ src/main/java/com/practice/problems/string/MaxLengthSubstring.java
@@ -0,0 +1,65 @@
+package com.practice.problems.string;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MaxLengthSubstring {
+    /**
+     * Longest length substring without repeating character
+     *
+     * @param str
+     * @return lengthOfLongestSubstringWithoutRepeatingChars
+     */
+    public int longestLengthSubstring(String str) {
+        str = str.toLowerCase();
+        str = str.replaceAll("\\s", "");//replacing in between spaces
+        System.out.println("trimmed string " + str);
+        Map<Character, Integer> seen = new HashMap<Character, Integer>();
+        int maxLen = 0;
+        int start = 0;
+        for (int end = 0; end < str.length(); end++) {
+            if (seen.containsKey(str.charAt(end))) {
+                start = Math.max(start, seen.get(str.charAt(end)) + 1);
+            }
+            seen.put(str.charAt(end), end);
+            maxLen = Math.max(maxLen, end - start + 1);
+        }
+        return maxLen;
+    }
+
+    /**
+     * Longest substring without repeating characters
+     */
+    public String longestSubstringWithoutRepeatingChars(String str) {
+        str=str.toLowerCase().trim();
+        int i;
+        int n = str.length();
+        int st = 0;
+        int currLen = 0;
+        int maxLen = 0;
+        int start = 0;
+        Map<Character, Integer> pos = new HashMap<Character, Integer>();
+        pos.put(str.charAt(0), 0);
+        for (i = 1; i < n; i++) {
+            if (!pos.containsKey(str.charAt(i))) {
+                pos.put(str.charAt(i), i);
+            } else {
+                if (pos.get(str.charAt(i)) >= st) {
+                    currLen = i - st;
+                    if (maxLen < currLen) {
+                        maxLen = currLen;
+                        start = st;
+                    }
+                    st = pos.get(str.charAt(i)) + 1;
+                }
+                pos.replace(str.charAt(i), i);
+            }
+        }
+        if (maxLen < i - st) {
+            maxLen = i - st;
+            start = st;
+        }
+        return str.substring(start, start + maxLen);
+    }
+
+}
---
+++ src/test/java/com/practice/problems/string/MaxLengthSubstringTest.java
@@ -0,0 +1,31 @@
+package com.practice.problems.string;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class MaxLengthSubstringTest {
+    MaxLengthSubstring maxLengthSubstring;
+    @Before
+    public void setUp(){
+        maxLengthSubstring = new MaxLengthSubstring();
+    }
+    @Test
+    public void testSentence(){
+
+        Assert.assertEquals(7,maxLengthSubstring.longestLengthSubstring("Abcad pqr"));
+    }
+
+    @Test
+    public void testWord(){
+        Assert.assertEquals(4,maxLengthSubstring.longestLengthSubstring("Abcad "));
+    }
+
+    @Test
+    public void testStringWord(){
+        Assert.assertEquals("abc",maxLengthSubstring.longestSubstringWithoutRepeatingChars("Abcaad"));
+    }
+}
